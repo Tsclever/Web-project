8172a552714414162b48b49cea87fa1866e74595
Tsclever <tsclever@outlook.com>
2024-09-02 19:58:52 +0800

    随机彩蛋生成

--- game/随机彩蛋生成/main.js
+++ game/随机彩蛋生成/main.js
@@ -25,9 +25,13 @@ function draw() {
   // .clearRect(x坐标, y坐标, 页面宽度, 页面高度), 实际作用: 清楚页面内容
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
   for (let i = 0; i < 100; i++) {
+    // beginPath()开始一个新的路径, 接下来可以使用Canvas提供方法进行 绘制
     ctx.beginPath();
+    // arc(): 用于绘制一个圆形, Math.PI: 用于表示π的值
+    // arc(圆心的x坐标, 圆心的y坐标, 圆的半径, 起始角度（弧度）, 结束角度（弧度）, 是否逆时针方向)
+    ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI, false);
     ctx.fillStyle = getRandomColor();
-    ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+    // 填充颜色, 与 fillStyle联动
     ctx.fill();
   }
 }
